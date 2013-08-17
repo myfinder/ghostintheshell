c59a96f991b591df03991c88db1d829070675770
Tatsuro Hisamori <medianetworks@gmail.com>
2013-08-17 18:43:47 +0900

    added template

--- lib/ghostintheshell/Web/Dispatcher.pm
+++ lib/ghostintheshell/Web/Dispatcher.pm
@@ -3,16 +3,31 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::Lite;
+use Data::Dump qw/dump/;
 
 any '/' => sub {
     my ($c) = @_;
     return $c->render('index.tt');
 };
 
-post '/account/logout' => sub {
+get '/campaign/add' => sub {
     my ($c) = @_;
-    $c->session->expire();
+    return $c->render('create_campaign.tt');
+};
+
+post '/campaign/add' => sub {
+    my ($c) = @_;
     return $c->redirect('/');
+};
+
+any '/campaign/list' => sub {
+    my ($c) = @_;
+    return $c->render('list_campaign.tt');
+};
+
+any '/tags/create' => sub {
+    my ($c) = @_;
+    return $c->render('create_tag.tt');
 };
 
 1;
